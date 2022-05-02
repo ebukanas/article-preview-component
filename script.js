7f692b77b5e630f1dde3fedb461883a4cc37108d
ebukanas <43076966+ebukanas@users.noreply.github.com>
2022-05-02 21:35:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,31 +5,34 @@ const shareBtn_LowRes = document.querySelector('#shareBtn_LowRes');
 const dropdown_LowRes = document.querySelector('#shareBtn_dropdown_low_res');
 const dropdown_HighRes = document.querySelector('#shareBtn_dropdown');
 
-//when you click on the share button on large resolutions it 
-//will change the dropdown display from none to flex
+var LargeScreenResolution = window.matchMedia('(min-width: 768px)');
+var SmallScreenResolution = window.matchMedia('(max-width: 768px)');
+
+//for larger resolutions
 shareBtn.addEventListener('click', () => {
     dropdown_LowRes.style.display = 'flex';
 
-//for larger resolutions
-var LargeScreenResolution = window.matchMedia('(min-width: 768px)');
     if (LargeScreenResolution.matches) {
         dropdown_LowRes.style.display = 'none';
         dropdown_HighRes.classList.toggle('show');
 }
 })
 
-// for smaller resolutions
-var SmallScreenResolution = window.matchMedia('(max-width: 768px)');
-if (SmallScreenResolution.matches) {
-    dropdown_HighRes.style.display = 'none';
-}
 
-//when you click on the share button on small resolutions it 
-//will change the dropdown display from flex to none
+// for smaller resolutions
 shareBtn_LowRes.addEventListener('click', () => {
     dropdown_LowRes.style.display = 'none';
+
+    if (SmallScreenResolution.matches) {
+        dropdown_HighRes.style.display = 'none';
+    }
 })
 
 
-
-
+window.addEventListener('resize', function(event) {
+    if (LargeScreenResolution.matches) {
+        dropdown_LowRes.style.display = 'none';
+    } else if (SmallScreenResolution.matches) {
+        dropdown_HighRes.style.display = 'none';
+    }
+}, true);
